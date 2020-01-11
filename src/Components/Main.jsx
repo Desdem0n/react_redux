74d8b3de7219a1293c49f44deab71ebf9dfe7121
Michael Milke <desmilke@gmail.com>
2020-01-11 14:54:19 +0100

    continued evaluation of app

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
 import Title from './Title';
+import PhotoWall from './PhotoWall'
+
+const posts = [{
+    id: "0",
+    description: "beautiful landscape",
+    imageLink: "https://image.jimcdn.com/app/cms/image/transf/none/"
+}, {
+    id: "1",
+    description: "Aliens???",
+    imageLink: "https://image.jimcdn.com/app/cms/image/transf/none/"
+ 
+},
+{
+    id: "2",
+    description: "On a vacation!",
+    imageLink: "https://image.jimcdn.com/app/cms/image/transf/none/"
+}]
+
 
 class Main extends Component {
     render() {
         return <div>
                  <Title title={'PhotoWall'}/>
- 
-
-                </div>
+                 <PhotoWall posts={posts}/>
+               </div>
     }
 }
 
